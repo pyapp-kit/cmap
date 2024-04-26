45d4cc1670a2ed8b5e92fa544d01251fc9dc2203
Jochem Smit <jhsmit@gmail.com>
2024-04-26 11:59:30 +0200

    add Paul Tol's colormaps

---
+++ src/cmap/data/tol/__init__.py
@@ -0,0 +1,435 @@
+"""These palettes are from Paul Tol's Notes on Colour schemes and templates. 
+
+Paul Tol is a Dutch instrument scientist with a PhD in atomic physics, 
+working on the TROPOMI, SPEXone and TANGO projects in the Earth programme of SRON.
+
+https://personal.sron.nl/~pault/
+https://personal.sron.nl/~pault/data/tol_colors.py
+https://personal.sron.nl/~pault/data/colourschemes.pdf
+
+Copyright:
+
+Copyright (c) 2022, Paul Tol
+All rights reserved.
+
+License:  Standard 3-clause BSD
+"""
+
+
+sunset_discrete = (
+    "#364B9A",
+    "#4A7BB7",
+    "#6EA6CD",
+    "#98CAE1",
+    "#C2E4EF",
+    "#EAECCC",
+    "#FEDA8B",
+    "#FDB366",
+    "#F67E4B",
+    "#DD3D2D",
+    "#A50026",
+)
+
+sunset = sunset_discrete
+
+nightfall_discrete = (
+    "#125A56",
+    "#238F9D",
+    "#60BCE9",
+    "#C6DBED",
+    "#ECEADA",
+    "#F9D576",
+    "#FD9A44",
+    "#E94C1F",
+    "#A01813",
+)
+
+nightfall = (
+    "#125A56",
+    "#00767B",
+    "#238F9D",
+    "#42A7C6",
+    "#60BCE9",
+    "#9DCCEF",
+    "#C6DBED",
+    "#DEE6E7",
+    "#ECEADA",
+    "#F0E6B2",
+    "#F9D576",
+    "#FFB954",
+    "#FD9A44",
+    "#F57634",
+    "#E94C1F",
+    "#D11807",
+    "#A01813",
+)
+
+BuRd_discrete = (
+    "#2166AC",
+    "#4393C3",
+    "#92C5DE",
+    "#D1E5F0",
+    "#F7F7F7",
+    "#FDDBC7",
+    "#F4A582",
+    "#D6604D",
+    "#B2182B",
+)
+
+BuRd = BuRd_discrete
+
+PRGn_discrete = (
+    "#762A83",
+    "#9970AB",
+    "#C2A5CF",
+    "#E7D4E8",
+    "#F7F7F7",
+    "#D9F0D3",
+    "#ACD39E",
+    "#5AAE61",
+    "#1B7837",
+)
+
+PRGn = PRGn_discrete
+
+YlOrBr_discrete = (
+    "#FFFFE5",
+    "#FFF7BC",
+    "#FEE391",
+    "#FEC44F",
+    "#FB9A29",
+    "#EC7014",
+    "#CC4C02",
+    "#993404",
+    "#662506",
+)
+
+YlOrBr = YlOrBr_discrete
+
+WhOrBr = (
+    "#FFFFFF",
+    "#FFF7BC",
+    "#FEE391",
+    "#FEC44F",
+    "#FB9A29",
+    "#EC7014",
+    "#CC4C02",
+    "#993404",
+    "#662506",
+)
+
+iridescent = (
+    "#FEFBE9",
+    "#FCF7D5",
+    "#F5F3C1",
+    "#EAF0B5",
+    "#DDECBF",
+    "#D0E7CA",
+    "#C2E3D2",
+    "#B5DDD8",
+    "#A8D8DC",
+    "#9BD2E1",
+    "#8DCBE4",
+    "#81C4E7",
+    "#7BBCE7",
+    "#7EB2E4",
+    "#88A5DD",
+    "#9398D2",
+    "#9B8AC4",
+    "#9D7DB2",
+    "#9A709E",
+    "#906388",
+    "#805770",
+    "#684957",
+    "#46353A",
+)
+
+incandescent = (
+    "#CEFFFF",
+    "#C6F7D6",
+    "#A2F49B",
+    "#BBE453",
+    "#D5CE04",
+    "#E7B503",
+    "#F19903",
+    "#F6790B",
+    "#F94902",
+    "#E40515",
+    "#A80003",
+)
+
+rainbow_PuRd = (
+    "#6F4C9B",
+    "#6059A9",
+    "#5568B8",
+    "#4E79C5",
+    "#4D8AC6",
+    "#4E96BC",
+    "#549EB3",
+    "#59A5A9",
+    "#60AB9E",
+    "#69B190",
+    "#77B77D",
+    "#8CBC68",
+    "#A6BE54",
+    "#BEBC48",
+    "#D1B541",
+    "#DDAA3C",
+    "#E49C39",
+    "#E78C35",
+    "#E67932",
+    "#E4632D",
+    "#DF4828",
+    "#DA2222",
+)
+
+rainbow_PuBr = (
+    "#6F4C9B",
+    "#6059A9",
+    "#5568B8",
+    "#4E79C5",
+    "#4D8AC6",
+    "#4E96BC",
+    "#549EB3",
+    "#59A5A9",
+    "#60AB9E",
+    "#69B190",
+    "#77B77D",
+    "#8CBC68",
+    "#A6BE54",
+    "#BEBC48",
+    "#D1B541",
+    "#DDAA3C",
+    "#E49C39",
+    "#E78C35",
+    "#E67932",
+    "#E4632D",
+    "#DF4828",
+    "#DA2222",
+    "#B8221E",
+    "#95211B",
+    "#721E17",
+    "#521A13",
+)
+
+rainbow_WhRd = (
+    "#E8ECFB",
+    "#DDD8EF",
+    "#D1C1E1",
+    "#C3A8D1",
+    "#B58FC2",
+    "#A778B4",
+    "#9B62A7",
+    "#8C4E99",
+    "#6F4C9B",
+    "#6059A9",
+    "#5568B8",
+    "#4E79C5",
+    "#4D8AC6",
+    "#4E96BC",
+    "#549EB3",
+    "#59A5A9",
+    "#60AB9E",
+    "#69B190",
+    "#77B77D",
+    "#8CBC68",
+    "#A6BE54",
+    "#BEBC48",
+    "#D1B541",
+    "#DDAA3C",
+    "#E49C39",
+    "#E78C35",
+    "#E67932",
+    "#E4632D",
+    "#DF4828",
+    "#DA2222",
+)
+
+rainbow_WhBr = (
+    "#E8ECFB",
+    "#DDD8EF",
+    "#D1C1E1",
+    "#C3A8D1",
+    "#B58FC2",
+    "#A778B4",
+    "#9B62A7",
+    "#8C4E99",
+    "#6F4C9B",
+    "#6059A9",
+    "#5568B8",
+    "#4E79C5",
+    "#4D8AC6",
+    "#4E96BC",
+    "#549EB3",
+    "#59A5A9",
+    "#60AB9E",
+    "#69B190",
+    "#77B77D",
+    "#8CBC68",
+    "#A6BE54",
+    "#BEBC48",
+    "#D1B541",
+    "#DDAA3C",
+    "#E49C39",
+    "#E78C35",
+    "#E67932",
+    "#E4632D",
+    "#DF4828",
+    "#DA2222",
+    "#B8221E",
+    "#95211B",
+    "#721E17",
+    "#521A13",
+)
+
+
+def _inject_rainbow_discrete():
+    clrs = [
+        "#E8ECFB",
+        "#D9CCE3",
+        "#D1BBD7",
+        "#CAACCB",
+        "#BA8DB4",
+        "#AE76A3",
+        "#AA6F9E",
+        "#994F88",
+        "#882E72",
+        "#1965B0",
+        "#437DBF",
+        "#5289C7",
+        "#6195CF",
+        "#7BAFDE",
+        "#4EB265",
+        "#90C987",
+        "#CAE0AB",
+        "#F7F056",
+        "#F7CB45",
+        "#F6C141",
+        "#F4A736",
+        "#F1932D",
+        "#EE8026",
+        "#E8601C",
+        "#E65518",
+        "#DC050C",
+        "#A5170E",
+        "#72190E",
+        "#42150A",
+    ]
+    indexes = [
+        [9],
+        [9, 25],
+        [9, 17, 25],
+        [9, 14, 17, 25],
+        [9, 13, 14, 17, 25],
+        [9, 13, 14, 16, 17, 25],
+        [8, 9, 13, 14, 16, 17, 25],
+        [8, 9, 13, 14, 16, 17, 22, 25],
+        [8, 9, 13, 14, 16, 17, 22, 25, 27],
+        [8, 9, 13, 14, 16, 17, 20, 23, 25, 27],
+        [8, 9, 11, 13, 14, 16, 17, 20, 23, 25, 27],
+        [2, 5, 8, 9, 11, 13, 14, 16, 17, 20, 23, 25],
+        [2, 5, 8, 9, 11, 13, 14, 15, 16, 17, 20, 23, 25],
+        [2, 5, 8, 9, 11, 13, 14, 15, 16, 17, 19, 21, 23, 25],
+        [2, 5, 8, 9, 11, 13, 14, 15, 16, 17, 19, 21, 23, 25, 27],
+        [2, 4, 6, 8, 9, 11, 13, 14, 15, 16, 17, 19, 21, 23, 25, 27],
+        [2, 4, 6, 7, 8, 9, 11, 13, 14, 15, 16, 17, 19, 21, 23, 25, 27],
+        [2, 4, 6, 7, 8, 9, 11, 13, 14, 15, 16, 17, 19, 21, 23, 25, 26, 27],
+        [1, 3, 4, 6, 7, 8, 9, 11, 13, 14, 15, 16, 17, 19, 21, 23, 25, 26, 27],
+        [1, 3, 4, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 19, 21, 23, 25, 26, 27],
+        [1, 3, 4, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 20, 22, 24, 25, 26, 27],
+        [
+            1,
+            3,
+            4,
+            6,
+            7,
+            8,
+            9,
+            10,
+            12,
+            13,
+            14,
+            15,
+            16,
+            17,
+            18,
+            20,
+            22,
+            24,
+            25,
+            26,
+            27,
+            28,
+        ],
+        [
+            0,
+            1,
+            3,
+            4,
+            6,
+            7,
+            8,
+            9,
+            10,
+            12,
+            13,
+            14,
+            15,
+            16,
+            17,
+            18,
+            20,
+            22,
+            24,
+            25,
+            26,
+            27,
+            28,
+        ],
+    ]
+
+    palettes = {
+        f"rainbow_discrete_{i + 1}": tuple(clrs[_idx] for _idx in idx)
+        for i, idx in enumerate(indexes)
+    }
+
+    globals().update(palettes)
+
+
+_inject_rainbow_discrete()
+
+bright = ("#4477AA", "#66CCEE", "#228833", "#CCBB44", "#EE6677", "#AA3377", "#BBBBBB")
+
+high_contrast = ("#DDAA33", "#BB5566", "#004488")
+
+vibrant = ("#0077BB", "#33BBEE", "#009988", "#EE7733", "#CC3311", "#EE3377", "#BBBBBB")
+
+muted = (
+    "#332288",
+    "#88CCEE",
+    "#44AA99",
+    "#117733",
+    "#999933",
+    "#DDCC77",
+    "#CC6677",
+    "#882255",
+    "#AA4499",
+)
+
+medium_contrast = ("#EECC66", "#EE99AA", "#6699CC", "#997700", "#994455", "#004488")
+
+pale = ("#BBCCEE", "#CCEEFF", "#CCDDAA", "#EEEEBB", "#FFCCCC", "#DDDDDD")
+
+dark = ("#222255", "#225555", "#225522", "#666633", "#663333", "#555555")
+
+light = (
+    "#7799DD",
+    "#99DDFF",
+    "#44BB99",
+    "#BBCC33",
+    "#AAAA00",
+    "#EEDD88",
+    "#EE8866",
+    "#FFAABB",
+    "#DDDDDD",
+)
