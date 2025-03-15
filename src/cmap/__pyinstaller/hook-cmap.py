f5c113e51a216b93507f27f8576932fa77927a36
Talley Lambert <talley.lambert@gmail.com>
2025-03-15 16:20:19 -0400

    feat: add pyinstaller hook

---
+++ src/cmap/__pyinstaller/hook-cmap.py
@@ -0,0 +1,22 @@
+from PyInstaller.utils.hooks import collect_all
+
+datas, binaries, hiddenimports = collect_all(
+    "cmap",
+    include_datas=["data/"],
+    exclude_datas=["**/__pycache__/"],
+    filter_submodules=lambda x: x.startswith("cmap.data"),
+)
+excludedimports = [
+    "bokeh",
+    "colorspacious",
+    "matplotlib",
+    "napari",
+    "numpy.typing",
+    "pydantic_core",
+    "pydantic",
+    "pygfx",
+    "pyqtgraph",
+    "typing_extensions",
+    "viscm",
+    "vispy",
+]
